5c0099f27a0118e5f19f0dba4b428d51479c5cd4
Muhammad Falak R Wani <falakreyaz@gmail.com>
2016-12-07 03:07:51 +0530

    Fiddle with some vars

---
+++ variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "initial"
+	fmt.Println(a)
+	a = "Next value"
+	b := "Some other way of strings"
+	var e, c int = 1, 2
+	fmt.Println(e, c)
+	fmt.Println(b)
+
+	d := true
+	fmt.Println(d)
+}
