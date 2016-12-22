e631ee6deee64cdf45e9c44a039dc429bcb792d3
Muhammad Falak R Wani <falakreyaz@gmail.com>
2016-12-23 00:29:15 +0530

    fileserver: Serve the current dir

--- fileserver.go
+++ fileserver.go
@@ -9,5 +9,5 @@ import (
 func main() {
 	// A static web server serving a path
 	fmt.Println("Server starting on Port 8080")
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/usr/share/doc"))))
+	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
 }
