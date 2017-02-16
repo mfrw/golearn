d18e838f4baa21acaa066d54425ad979eadd0236
Muhammad Falak R Wani <falakreyaz@gmail.com>
2017-02-16 19:22:09 +0530

    fileserver: modify comments

--- fileserver.go
+++ fileserver.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	// A static web server serving a path
+	// A static web server serving a current path
 	fmt.Println("Server starting on Port 8080")
 	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
 }
