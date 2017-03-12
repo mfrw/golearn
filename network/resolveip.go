1a92a9a97235541439c3a8313c3415c2204a2b3b
Muhammad Falak R Wani <falakreyaz@gmail.com>
2017-03-12 17:08:27 +0530

    network: resolve ip in go

---
+++ network/resolveip.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s hostname\n", os.Args[0])
+		os.Exit(1)
+	}
+	name := os.Args[1]
+	addr, err := net.ResolveIPAddr("ip", name)
+	if err != nil {
+		fmt.Println("Resolution Error", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Resolved addr is ", addr.String())
+	os.Exit(0)
+}
